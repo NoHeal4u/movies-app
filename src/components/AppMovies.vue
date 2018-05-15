971e2dc27317a5e1342055bafd16e96c5f5ca74a
NoHeal4u <miodragnikolic@yahoo.com>
2018-05-15 19:23:46 +0200

    uradjen prvi zadatak

---
+++ src/components/AppMovies.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+	<p>AppMovies</p>
+</div>
+	
+</template>
+
+<script>
+	
+	export default {
+
+	}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
+import VueRouter from 'vue-router'
 import Vue from 'vue'
 import App from './App.vue'
+import AppMovies from './components/AppMovies.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [
+	{path: '/', redirect: '/movies'},
+	{path: '/movies', component: AppMovies , name: 'movies'},
+]
+
+const router = new VueRouter({
+	routes,
+	mode: 'history'
+})
+
 new Vue({
+	router,
   render: h => h(App)
 }).$mount('#app')
